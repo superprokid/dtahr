96f5750347a0301c80c069bf1e3fa76f0a39ff66
Le Duc Thang <67146524+ldthang2201@users.noreply.github.com>
2022-09-24 11:37:35 +0700

    Update asyncRecallFunction

--- web/src/services/API/API.js
+++ web/src/services/API/API.js
@@ -1,11 +1,11 @@
 import axiosBase from 'axios';
 import SessionUtls from '../SessionUtls';
 import {
-	// USER_REFRESH_TOKEN_URL,
-	// LIMIT_RECALL_API,
+	USER_REFRESH_TOKEN_URL,
+	LIMIT_RECALL_API,
 } from '../../config/constant';
 
-// let asyncRecallNum = 0;
+let asyncRecallNum = 0;
 
 function getAccessTokenHeader() {
 	axiosBase.defaults.headers.common[
@@ -33,31 +33,46 @@ const axiosClient = {
 	},
 };
 
-// async function refreshToken() {
-// 	const refreshToken = SessionUtls.getRefreshToken();
-// 	if (!refreshToken) {
-// 		return false;
-// 	}
-// 	const response = await axiosClient.post(USER_REFRESH_TOKEN_URL, {
-// 		refreshToken,
-// 	});
-// 	if (response.status === 200) {
-// 		SessionUtls.setAccessToken(response.data.accessToken);
-// 		return true;
-// 	} else {
-// 		SessionUtls.clearLoginSession();
-// 		return false;
-// 	}
-// }
+async function refreshToken() {
+	const refreshToken = SessionUtls.getRefreshToken();
+	if (!refreshToken) {
+		return false;
+	}
+	const response = await axiosClient.post(USER_REFRESH_TOKEN_URL, {
+		refreshToken,
+	});
+	if (response.status === 200) {
+		SessionUtls.setAccessToken(response.data.accessToken);
+		return true;
+	} else {
+		SessionUtls.clearLoginSession();
+		return false;
+	}
+}
 
 /**
  * Recall api if needed
  * @param {Promise} request
  */
-export async function asyncRecallFunction(apiFunction) {
-    console.log(apiFunction)
-    let result = await apiFunction()
-    console.log(result)
+export function asyncRecallFunction(apiFunction) {
+	return apiFunction().then(async (result) => {
+		asyncRecallNum = 0;
+		return result;
+	}).catch(async error => {
+		
+		if (error.response.status === 401) {
+			await refreshToken();
+			return await apiFunction();
+		}
+
+		if (error.response.status === 403) {
+			if (asyncRecallNum >= LIMIT_RECALL_API) {
+				return null;
+			}
+			asyncRecallNum++;
+			return asyncRecallFunction(apiFunction);
+		}
+	})
 }
 
 export default axiosClient;
--- web/src/views/ClientSide/Home/Home.js
+++ web/src/views/ClientSide/Home/Home.js
@@ -7,10 +7,9 @@ export default {
 	name: 'HomePage',
 	methods: {
 		async test() {
-			let response = await asyncRecallFunction(() => {
-				return axiosClient.get(BASE_API_USER_URL + '/get');
+			await asyncRecallFunction(() => {
+				return axiosClient.get(BASE_API_USER_URL + '/get')
 			});
-			console.log(response);
 		},
 	},
 };
