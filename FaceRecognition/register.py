c772093f2cba0a286ac98880e49fa98469e45edf
buivietduy <99153490+buivietduy@users.noreply.github.com>
2022-10-28 09:41:07 +0700

    :+1: Update Gitignore + remove HTTPS certificate

--- FaceRecognition/register.py
+++ FaceRecognition/register.py
@@ -24,9 +24,11 @@ app = Flask(
     static_folder="../client/dist/static",
     template_folder="../client/dist",
 )
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-context.load_cert_chain('cert.crt', 'server_secret_wo_pass.key')
-ssl._create_default_https_context = ssl._create_unverified_context
+
+# For HTTPS Only
+# context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+# context.load_cert_chain('cert.crt', 'server_secret_wo_pass.key')
+# ssl._create_default_https_context = ssl._create_unverified_context
 
 # Instance
 face_engine = FaceRecognitionLib()
@@ -81,4 +83,5 @@ def add_staff():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", ssl_context=context)
+    app.run(debug=True, host="0.0.0.0")
+    # app.run(debug=True, host="0.0.0.0", ssl_context=context)
