42bdd914546144e40305fa4f3edfc60368456f49
buivietduy <99153490+buivietduy@users.noreply.github.com>
2022-11-26 13:46:50 +0700

    :+1: Update CSV Export

---
+++ web/src/views/AdminSide/AdminCSVExport/AdminCSVExport.js
@@ -0,0 +1,36 @@
+export default {
+  name: 'AdminCSVExport',
+  data() {
+    return {
+      csvSelect: 0,
+      monthSelect: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      csvOption: [
+        {
+          name: 'Salary',
+          value: 0,
+        },
+        {
+          name: 'Overtime',
+          value: 1,
+        },
+        {
+          name: 'Absent',
+          value: 2,
+        },
+        {
+          name: 'Holiday',
+          value: 3,
+        }
+      ]
+    }
+  },
+  methods: {
+    allowedMonths(value) {
+      const date = new Date(value);
+      return date <= new Date();
+    },
+    exportCSV() {
+      
+    }
+  }
+}
---
+++ web/src/views/AdminSide/AdminCSVExport/AdminCSVExport.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-radio-group
+      v-model="csvSelect"
+      row
+    >
+      <v-radio
+        v-for="(option) in csvOption"
+        :key="option.value"
+        :label="option.name"
+        :value="option.value"
+      ></v-radio>
+    </v-radio-group>
+    <v-date-picker
+      v-model="monthSelect"
+      type="month"
+      :allowed-dates="allowedMonths"
+    ></v-date-picker>
+    <!-- <v-date-picker
+      v-model="monthSelect"
+      type="year"
+      :allowed-dates="allowedMonths"
+    ></v-date-picker> -->
+  </div>
+</template>
+
+<script src="./AdminCSVExport.js">
+</script>
+
+<style src="./AdminCSVExport.css">
+</style>
